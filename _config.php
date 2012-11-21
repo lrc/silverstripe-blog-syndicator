43a760819ec2d687ca2bae6914eb54322e601f9d
Simon Elvery <simon@elvery.net>
2012-11-22 09:07:36 +1000

    Add dependency check.

--- _config.php
+++ _config.php
@@ -1,4 +1,15 @@
 <?php
 
+// Dependency checking
+if(!ClassInfo::exists('CronTab')) {
+	$view = new DebugView();
+	if(!headers_sent()) header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error');
+	$view->writeHeader();
+	$view->writeInfo('Dependency Error', 'The blog syndicator module requires the pseudo cron module.');
+	$view->writeParagraph('Please install the <a href="https://github.com/lrc/silverstripe-pseudo-cron">pseudo cron</a> module.');
+	$view->writeFooter();
+	exit;
+}
+
 Object::add_extension('BlogHolder', 'BlogSyndicatorHolderExtension');
 Object::add_extension('BlogEntry', 'BlogSyndicatorEntryExtension');
